2acc5618843954dd5e433926099d0d297eabb483
litmus4 <scrudlds@163.com>
2015-11-22 11:32:08 +0800

    SpriteProduct

---
+++ CC37TmplProject/Code/PxcCore/AssetsProducer/SpriteProduct.cpp
@@ -0,0 +1,134 @@
+#include "SpriteProduct.h"
+
+CSpriteProduct::CSpriteProduct()
+{
+	m_bSpriteFrame = false;
+	m_pTmpl = NULL;
+
+	m_pSprite = NULL;
+}
+
+CSpriteProduct::~CSpriteProduct()
+{
+	//
+}
+
+bool CSpriteProduct::IsSpriteFrame()
+{
+	return m_bSpriteFrame;
+}
+
+std::string CSpriteProduct::GetName()
+{
+	return m_strName;
+}
+
+bool CSpriteProduct::Read(PxcUtil::CCSVTableOperator& tabop)
+{
+	if (!tabop.GetValue("ID", m_iID))
+		return false;
+
+	int isf = 0;
+	if (tabop.GetValue("IsSpriteFrame", isf))
+		m_bSpriteFrame = (bool)isf;
+	else
+		return false;
+
+	if (m_bSpriteFrame)
+	{
+		if (!tabop.GetValue("PlistFileName", m_strPlistFileName))
+			return false;
+
+		if (!tabop.GetValue("TexFileName", m_strAtlasFileName))
+			return false;
+
+		if (!tabop.GetValue("FrameName", m_strName))
+			return false;
+	}
+	else
+	{
+		if (!tabop.GetValue("TexFileName", m_strName))
+			return false;
+	}
+
+	return true;
+}
+
+bool CSpriteProduct::Load()
+{
+	if (IsLoaded())
+		return true;
+	if (m_eLoadType == ESharedData)
+		return false;
+
+	if (m_pTmpl == NULL)
+	{
+		if (m_bSpriteFrame)
+		{
+			cocos2d::SpriteFrameCache::getInstance()->addSpriteFramesWithFile(m_strPlistFileName, m_strAtlasFileName);
+			m_pSprite = cocos2d::Sprite::createWithSpriteFrameName(m_strName);
+		}
+		else
+			m_pSprite = cocos2d::Sprite::create(m_strName);
+	}
+	else if (m_pTmpl->GetSprite())
+	{
+		if (m_bSpriteFrame)
+		{
+			cocos2d::SpriteFrame* pFrame = m_pTmpl->GetSprite()->getSpriteFrame();
+			m_pSprite = cocos2d::Sprite::createWithSpriteFrame(pFrame);
+		}
+		else
+		{
+			cocos2d::Texture2D* pTex = m_pTmpl->GetSprite()->getTexture();
+			m_pSprite = cocos2d::Sprite::createWithTexture(pTex);
+		}
+	}
+	else
+		return false;
+	if (m_pSprite)
+		m_pSprite->retain();
+
+	CBaseProduct::Load();
+	return true;
+}
+
+void CSpriteProduct::UnLoad()
+{
+	if (IsLoaded() && m_pSprite)
+	{
+		m_pSprite->release();
+	}
+}
+
+CBaseProduct* CSpriteProduct::Clone()
+{
+	CSpriteProduct* pProduct = new CSpriteProduct();
+	pProduct->m_iID = m_iID;
+	pProduct->m_strName = m_strName;
+	pProduct->m_bSpriteFrame = m_bSpriteFrame;
+	pProduct->m_strPlistFileName = m_strPlistFileName;
+	pProduct->m_strAtlasFileName = m_strAtlasFileName;
+	pProduct->m_pTmpl = m_pTmpl;
+	return pProduct;
+}
+
+void CSpriteProduct::Update(float dt)
+{
+	//TODO
+}
+
+void CSpriteProduct::Draw()
+{
+	//TODO
+}
+
+cocos2d::Sprite* CSpriteProduct::GetSprite()
+{
+	return m_pSprite;
+}
+
+void CSpriteProduct::OnLoadComplete()
+{
+	//TODO
+}
---
+++ CC37TmplProject/Code/PxcCore/AssetsProducer/SpriteProduct.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "cocos2d.h"
+#include "BaseProduct.h"
+
+class CSpriteProduct : public CBaseProduct
+{
+public:
+	CSpriteProduct();
+	virtual ~CSpriteProduct();
+
+	bool IsSpriteFrame();
+	std::string GetName();
+
+	virtual bool Read(PxcUtil::CCSVTableOperator& tabop);
+	virtual bool Load();
+	virtual void UnLoad();
+	virtual CBaseProduct* Clone();
+
+	void Update(float dt);
+	void Draw();
+	cocos2d::Sprite* GetSprite();
+
+	virtual void OnLoadComplete();
+
+private:
+	std::string m_strName;
+	bool m_bSpriteFrame;
+	std::string m_strPlistFileName;
+	std::string m_strAtlasFileName;
+	CSpriteProduct* m_pTmpl;
+
+	cocos2d::Sprite* m_pSprite;
+};

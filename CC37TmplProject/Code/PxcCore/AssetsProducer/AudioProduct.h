c4b58af86f77440b0d72ae3689432981724501be
litmus4 <scrudlds@163.com>
2016-04-07 17:51:39 +0800

    AudioProduct(只写了头文件)

---
+++ CC37TmplProject/Code/PxcCore/AssetsProducer/AudioProduct.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "BaseProduct.h"
+
+class CAudioProduct : public CBaseProduct
+{
+public:
+	struct Ident
+	{
+		std::string strName;
+		unsigned int uSoundID;
+	};
+
+	struct SharedSoundItem
+	{
+		Ident ident;
+		CAudioProduct* pLink;
+		float fTime;
+	};
+
+public:
+	CAudioProduct();
+	~CAudioProduct();
+
+	std::string GetName();
+	bool IsBgm();
+	unsigned int GetSoundID();
+
+	virtual bool Read(PxcUtil::CCSVTableOperator& tabop);
+	virtual bool Load();
+	virtual void UnLoad();
+	virtual CBaseProduct* Clone();
+
+	void Update(float dt);
+	void Play(bool bLoop);
+	void Stop();
+	bool IsPlaying();
+	void SetVolume(float fVol);
+	float GetVolume();
+	void SetMuteExternally(bool b);
+	void SetVolumeExternally(float fVol);
+
+	virtual void OnLoadComplete();
+
+	Ident& CreateSharedSound(CAudioProduct* pLink);
+	Ident& ReloadRelation(Ident& ident, CAudioProduct* pLink);
+	bool CheckAndCutLink(Ident& ident, CAudioProduct* pLink);
+
+private:
+	Ident& SelectBestSharedSound(bool pbMax, CAudioProduct* pRobLink);
+
+private:
+	Ident m_Ident;
+	bool m_bBgm;
+	float m_fFirmVolume;//表中填写的固定音量
+	CAudioProduct* m_pTmpl;
+
+	std::vector<SharedSoundItem> m_vecSharedSounds;
+
+	bool m_bPlaying;
+	bool m_bLoop;
+	float m_fVolume;
+	bool m_bMuteEx;
+	bool m_fVolumeEx;
+};

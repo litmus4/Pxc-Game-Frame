3c4dc4bb35298b3b04fc643ee58848a0a9cbf62a
litmus4 <scrudlds@163.com>
2016-11-23 17:25:00 +0800

    增加控制台，方便下一步调试

--- CC37TmplProject/Code/proj.win32/main.cpp
+++ CC37TmplProject/Code/proj.win32/main.cpp
@@ -11,8 +11,18 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+#ifdef _DEBUG
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONIN$", "r", stdin);
+#endif
 
     // create the application instance
     AppDelegate app;
     return Application::getInstance()->run();
+
+#ifdef _DEBUG
+	FreeConsole();
+#endif
+
 }

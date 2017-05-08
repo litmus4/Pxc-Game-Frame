a64302b7338a81f53cdac959c4c77e7ac92e3229
litmus4 <scrudlds@163.com>
2017-05-08 23:10:44 +0800

    继续ArmatureProduct.h 170508b

--- CC37TmplProject/Code/PxcCore/AssetsProducer/ArmatureProduct.h
+++ CC37TmplProject/Code/PxcCore/AssetsProducer/ArmatureProduct.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "cocos2d.h"
+#include "cocostudio\CocoStudio.h"
 #include "BaseProduct.h"
 #include "ArmatureActionInfo.h"
 #include "PxcUtil\Delegate.h"
@@ -82,4 +83,32 @@ public:
 		int iCurIndex;
 		std::vector<CArmatureActionInfo*> vecActions;
 	};
+
+public:
+	CArmatureProduct();
+	virtual ~CArmatureProduct();
+
+protected:
+	//
+
+private:
+	std::string m_strConfigFileName;
+	std::string m_strPlistFileName;
+	std::string m_strImageFileName;
+	std::string m_strActionFileName;
+	CArmatureProduct* m_pTmpl;
+
+	CSubThreadDataReader* m_pReader;
+	cocostudio::Armature* m_pArmature;
+	typedef std::map<CArmatureActionInfo::EDynamicSkinType,
+		std::map<int/*ActionID*/, ActionGroup>> tSubSkinMap;
+	typedef std::map<CArmatureActionInfo::EParentBinderType, tSubSkinMap> tMainBinderMap;
+	tMainBinderMap m_mapActions;
+	float m_fAnimInternal;
+	cocos2d::Vec4 m_v4Trans;
+
+	std::list<RequestAction> m_lisRequestActions;
+	CurrentAction m_CurrentActions[EPriority_Num];
+	PartAction m_PartActions[EPart_Num];
+	std::string m_strPartBoneNames[3];
 };

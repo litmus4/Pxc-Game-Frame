ea25dbaf001233e6873028b4202f2a0adf16b911
litmus4 <scrudlds@163.com>
2019-12-22 19:32:34 +0800

    尝试给项目引用库，报错：��ǰ�������в��������ơ�Path��

--- UE4TmplProject/Build/Source/UE4Game/UE4Game.Build.cs
+++ UE4TmplProject/Build/Source/UE4Game/UE4Game.Build.cs
@@ -4,6 +4,30 @@ using UnrealBuildTool;
 
 public class UE4Game : ModuleRules
 {
+    public string PxcLibPath
+    {
+        get
+        {
+            return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../../../Shared/PxcLibs"));
+        }
+    }
+
+    public string PxcCorePath
+    {
+        get
+        {
+            return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../PxcCore"));
+        }
+    }
+
+    public string MidPath
+    {
+        get
+        {
+            return Path.GetFullPath(Path.Combine(ModuleDirectory, "../../Intermediate"));
+        }
+    }
+
 	public UE4Game(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -12,12 +36,21 @@ public class UE4Game : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+
+        PublicIncludePaths.Add(Path.Combine(PxcLibPath, ""));
+        PublicIncludePaths.Add(Path.Combine(PxcCorePath, ""));
+        string CfgStr = (Target.Configuration == UnrealTargetConfiguration.Shipping ? "Release" : "Debug");
+        PublicAdditionalLibraries.Add(Path.Combine(PxcLibPath, CfgStr, "zpack.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(PxcLibPath, CfgStr, "PxcUtil.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(PxcLibPath, CfgStr, "tinyxml.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(MidPath, CfgStr + ".win32", "PublicDefinitions.lib"));
+        PublicAdditionalLibraries.Add(Path.Combine(MidPath, CfgStr + ".win32", "DataTables.lib"));
+    }
 }

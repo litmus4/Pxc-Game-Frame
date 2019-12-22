6c117f91386ef3845f9cb8cd2be3a3f848e2c0b5
litmus4 <scrudlds@163.com>
2019-12-22 16:58:43 +0800

    创建UE4TmplProject，新的开始！

---
+++ UE4TmplProject/Build/Source/UE4Game/UE4Game.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "UE4Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4Game, "UE4Game" );
---
+++ UE4TmplProject/Build/Source/UE4Game/UE4GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4GAME_API AUE4GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

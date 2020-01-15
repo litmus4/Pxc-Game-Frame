cb690c438735209a28ca6473114836c6c0e3110c
litmus4 <scrudlds@163.com>
2020-01-15 13:01:36 +0800

    PublicAdditionalLibraries无效？

--- UE4TmplProject/Construct/Source/UE4Game/UE4GameGameModeBase.cpp
+++ UE4TmplProject/Construct/Source/UE4Game/UE4GameGameModeBase.cpp
@@ -2,6 +2,21 @@
 
 
 #include "UE4GameGameModeBase.h"
+#include "Misc/Paths.h"
+#include "Containers/StringConv.h"
+
+#include "Windows/AllowWindowsPlatformTypes.h"
+#include "Windows/PreWindowsApi.h"
+#include "PxcUtil/LogCenter.h"
+#include "PublicDefinitions/SpecialFileDef.h"
+//#include "DataTables/TextTable/TextTableCenter.h"
+//#include "DataTables/OtherTable/OtherTableCenter.h"
+#include "Windows/PostWindowsApi.h"
+#include "Windows/HideWindowsPlatformTypes.h"
+//#include "PxcUtil/zPackEx.h"
+#include <string>
+
+#define USE_ZPACK 2
 
 AUE4GameGameModeBase::AUE4GameGameModeBase()
 {
@@ -12,5 +27,7 @@ void AUE4GameGameModeBase::InitGame(const FString& MapName, const FString& Optio
 {
 	AGameModeBase::InitGame(MapName, Options, ErrorMessage);
 
-	//FLAGJK
+	std::string strDir = TCHAR_TO_ANSI(*FPaths::ProjectSavedDir());
+	PXCU_LOGINST->Init(SpecialFileDef::ELogFile_AssetsTables, (strDir + "Logs\\log_assetstables.txt").c_str());
+	//
 }

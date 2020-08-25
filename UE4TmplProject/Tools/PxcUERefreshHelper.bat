90286c4675e0f70d6855eb2394074cb3e886c230
litmus4 <scrudlds@163.com>
2020-08-25 22:00:17 +0800

    PxcUERefreshHelper适配成功

---
+++ UE4TmplProject/Tools/PxcUERefreshHelper.bat
@@ -0,0 +1,2 @@
+%~dp0..\..\..\PxcUERefreshHelper\Release\PxcUERefreshHelper.exe %~dp0..\Construct\UE4Game.sln
+::%~dp0..\..\CommonTools\For-UnrealEngine\PxcUERefreshHelper.exe %~dp0..\Construct\UE4Game.sln

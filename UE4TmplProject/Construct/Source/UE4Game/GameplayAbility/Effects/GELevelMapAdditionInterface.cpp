84efe9ec2bfac4b0d210f946447df9730b01969b
litmus4 <scrudlds@163.com>
2021-02-20 17:18:45 +0800

    GELevelMapAdditionInterface

---
+++ UE4TmplProject/Construct/Source/UE4Game/GameplayAbility/Effects/GELevelMapAdditionInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameplayAbility/Effects/GELevelMapAdditionInterface.h"
+
+// Add default functionality here for any IGELevelMapAdditionInterface functions that are not pure virtual.
---
+++ UE4TmplProject/Construct/Source/UE4Game/GameplayAbility/Effects/GELevelMapAdditionInterface.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GELevelMapAdditionInterface.generated.h"
+
+class UAbilitySystemComponent;
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UGELevelMapAdditionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class UE4GAME_API IGELevelMapAdditionInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void GetAdditionInCurrentLevel(UAbilitySystemComponent* pASC, TMap<FString, float>& tmapOutAddition);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void GetAdditionByLevel(UAbilitySystemComponent* pASC, int32 iLevel, TMap<FString, float>& tmapOutAddition);
+};

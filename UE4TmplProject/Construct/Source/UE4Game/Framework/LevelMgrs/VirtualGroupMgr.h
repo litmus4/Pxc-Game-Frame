508f07ffa57b2be5ff817109f78c13737508ecc0
litmus4 <scrudlds@163.com>
2021-03-02 17:59:13 +0800

    空的VirtualGroupMgr

---
+++ UE4TmplProject/Construct/Source/UE4Game/Framework/LevelMgrs/VirtualGroupMgr.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "VirtualGroupMgr.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4GAME_API UVirtualGroupMgr : public UObject
+{
+	GENERATED_BODY()
+	
+};

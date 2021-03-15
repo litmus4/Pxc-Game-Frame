ac583bb433476751b650cc3923b5cc958cd7724c
litmus4 <scrudlds@163.com>
2021-03-15 17:46:40 +0800

    继续RelativeTimeDilationMgr

--- UE4TmplProject/Construct/Source/UE4Game/Framework/LevelMgrs/RelativeTimeDilationMgr.cpp
+++ UE4TmplProject/Construct/Source/UE4Game/Framework/LevelMgrs/RelativeTimeDilationMgr.cpp
@@ -3,3 +3,83 @@
 
 #include "Framework/LevelMgrs/RelativeTimeDilationMgr.h"
 
+FTimeDilationInfo::FTimeDilationInfo()
+	: fDuration(0.0f), fBlendInTime(0.0f), fBlendOutTime(0.0f)
+	, fStaticDilation(0.0f), pDynamicDilation(nullptr)
+	, eLevel(ERTDilationLevel::AffectActor), pAffectActor(nullptr), bIgnoreParent(false)
+	, iPriority(-1)
+	, fCurBlendInTime(-1.0f), fCurBlendOutTime(-1.0f), fRemainingTime(0.0f)
+{
+	//
+}
+
+void FTimeDilationInfo::InitGlobal(float xDuration, float xBlendInTime, float xBlendOutTime,
+	float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority)
+{
+	eLevel = ERTDilationLevel::Global;
+	AffectGroupName = NAME_None;
+	pAffectActor = nullptr;
+	bIgnoreParent = false;
+
+	InitUniversal(xDuration, xBlendInTime, xBlendOutTime, xStaticDilation, xDynamicDilation, xPriority);
+}
+
+void FTimeDilationInfo::InitGroup(const FName& GroupName, float xDuration, float xBlendInTime, float xBlendOutTime,
+	float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority, bool xIgnoreParent)
+{
+	eLevel = ERTDilationLevel::AffectGroup;
+	AffectGroupName = GroupName;
+	pAffectActor = nullptr;
+	bIgnoreParent = xIgnoreParent;
+
+	InitUniversal(xDuration, xBlendInTime, xBlendOutTime, xStaticDilation, xDynamicDilation, xPriority);
+}
+
+void FTimeDilationInfo::InitActor(AActor* pActor, float xDuration, float xBlendInTime, float xBlendOutTime,
+	float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority, bool xIgnoreParent)
+{
+	eLevel = ERTDilationLevel::AffectActor;
+	AffectGroupName = NAME_None;
+	pAffectActor = pActor;
+	bIgnoreParent = xIgnoreParent;
+
+	InitUniversal(xDuration, xBlendInTime, xBlendOutTime, xStaticDilation, xDynamicDilation, xPriority);
+}
+
+void FTimeDilationInfo::InitUniversal(float xDuration, float xBlendInTime, float xBlendOutTime,
+	float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority)
+{
+	fDuration = xDuration;
+	fBlendInTime = FMath::Max(xBlendInTime, 0.0f);
+	fBlendOutTime = FMath::Max(xBlendOutTime, 0.0f);
+
+	fStaticDilation = xStaticDilation;
+	pDynamicDilation = xDynamicDilation;
+
+	iPriority = xPriority;
+
+	if (fDuration <= 0.0f && fDuration >= -0.5f)
+		return;
+
+	if (fRemainingTime > 0.0f)
+	{
+		fRemainingTime = fDuration;
+
+		if (fCurBlendOutTime >= -0.5f)
+		{
+			//fCurBlendOutTime = 0.0f;
+			if (xBlendInTime > 0.0f)
+				fCurBlendInTime = fBlendInTime * FMath::Max(1.0f - fCurBlendOutTime / fBlendOutTime, 0.0f);
+			else
+				fCurBlendInTime = -1.0f;
+			fCurBlendOutTime = -1.0f;
+		}
+	}
+	else
+	{
+		fRemainingTime = fDuration;
+
+		if (xBlendInTime > 0.0f)
+			fCurBlendInTime = 0.0f;
+	}
+}
--- UE4TmplProject/Construct/Source/UE4Game/Framework/LevelMgrs/RelativeTimeDilationMgr.h
+++ UE4TmplProject/Construct/Source/UE4Game/Framework/LevelMgrs/RelativeTimeDilationMgr.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "PrivateDefinitions/LevelManagersDef.h"
+#include <string>
 #include "RelativeTimeDilationMgr.generated.h"
 
 USTRUCT()
@@ -12,9 +13,38 @@ struct FTimeDilationInfo
 {
 	GENERATED_BODY()
 public:
-	//FTimeDilationInfo();
+	FTimeDilationInfo();
 
-	//void Init();//FLAGJK
+	void InitGlobal(float xDuration, float xBlendInTime, float xBlendOutTime,
+		float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority);
+	void InitGroup(const FName& GroupName, float xDuration, float xBlendInTime, float xBlendOutTime,
+		float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority, bool xIgnoreParent);
+	void InitActor(AActor* pActor, float xDuration, float xBlendInTime, float xBlendOutTime,
+		float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority, bool xIgnoreParent);
+
+	float fDuration;//<-0.5 >0.0
+	float fBlendInTime;
+	float fBlendOutTime;
+
+	float fStaticDilation;
+	UPROPERTY()
+	UCurveFloat* pDynamicDilation;
+
+	ERTDilationLevel eLevel;
+	FName AffectGroupName;
+	UPROPERTY()
+	AActor* pAffectActor;
+	bool bIgnoreParent;
+
+	int32 iPriority;
+
+private:
+	void InitUniversal(float xDuration, float xBlendInTime, float xBlendOutTime,
+		float xStaticDilation, UCurveFloat* xDynamicDilation, int32 xPriority);
+
+	float fCurBlendInTime;//<-0.5 >0.0
+	float fCurBlendOutTime;//<-0.5 >0.0
+	float fRemainingTime;//<-0.5 >0.0
 };
 
 /**
@@ -25,4 +55,10 @@ class UE4GAME_API URelativeTimeDilationMgr : public UObject
 {
 	GENERATED_BODY()
 	
+	/*FLAGJK
+	 * std::map<int64, int32> m_mapComplexUids; eLevel << 32 | iUid, iUid
+	 * TMap<int32, FTimeDilationData> m_tmapTimeDilations;
+	 * std::unordered_map<uint32, int32> m_mapHashToUids;
+	 * std::unordered_map<ERTDilationLevel, int32> m_mapLevelNums;
+	 */
 };

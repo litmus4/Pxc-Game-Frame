2d0a7312c627f4a202a21d54a45211cc7ec59757
litmus4 <scrudlds@163.com>
2021-05-10 17:32:20 +0800

    TestTriggeredActor

---
+++ UE4TmplProject/Construct/Source/UE4Game/Tests/TestTriggeredActor.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tests/TestTriggeredActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ATestTriggeredActor::ATestTriggeredActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	m_pMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	m_pMeshComp->SetSimulatePhysics(true);
+	m_pMeshComp->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
+	m_pMeshComp->SetCollisionResponseToAllChannels(ECR_Block);
+	RootComponent = m_pMeshComp;
+
+	m_pCppBoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("CppBoxComp"));
+	m_pCppBoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	m_pCppBoxComp->SetCollisionResponseToAllChannels(ECR_Overlap);
+	m_pCppBoxComp->OnComponentBeginOverlap.AddDynamic(this, &ATestTriggeredActor::OnBoxOverlapped);
+	m_pCppBoxComp->SetupAttachment(RootComponent);
+
+	m_pBpBoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BpBoxComp"));
+	m_pBpBoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	m_pBpBoxComp->SetCollisionResponseToAllChannels(ECR_Overlap);
+	m_pBpBoxComp->OnComponentBeginOverlap.AddDynamic(this, &ATestTriggeredActor::OnBoxOverlapped);
+	m_pBpBoxComp->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ATestTriggeredActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATestTriggeredActor::OnBoxOverlapped(UPrimitiveComponent* pOverlappedComp, AActor* pOtherActor, UPrimitiveComponent* pOtherComp,
+	int32 iOtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	check(pOverlappedComp);
+	FString&& sCompName = pOverlappedComp->GetName();
+	if (sCompName == TEXT("CppBoxComp"))
+	{
+		//
+	}
+	else if (sCompName == TEXT("BpBoxComp"))
+	{
+		//
+	}
+}
+
+// Called every frame
+void ATestTriggeredActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ UE4TmplProject/Construct/Source/UE4Game/Tests/TestTriggeredActor.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestTriggeredActor.generated.h"
+
+class UStaticMeshComponent;
+class UBoxComponent;
+
+UCLASS()
+class UE4GAME_API ATestTriggeredActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATestTriggeredActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components)
+	UStaticMeshComponent* m_pMeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components)
+	UBoxComponent* m_pCppBoxComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components)
+	UBoxComponent* m_pBpBoxComp;
+
+	UFUNCTION()
+	void OnBoxOverlapped(UPrimitiveComponent* pOverlappedComp, AActor* pOtherActor, UPrimitiveComponent* pOtherComp,
+		int32 iOtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};

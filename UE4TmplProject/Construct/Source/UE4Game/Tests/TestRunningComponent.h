a2fbef97895f671342ef6f100aa0bc6e27165b6b
litmus4 <scrudlds@163.com>
2021-05-11 17:48:10 +0800

    开始TestRunningComponent，后边要+Blueprintable

---
+++ UE4TmplProject/Construct/Source/UE4Game/Tests/TestRunningComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TestRunningComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UE4GAME_API UTestRunningComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTestRunningComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void RunCppTestNoParam() {}
+
+	UFUNCTION(BlueprintNativeEvent)
+	void RunBPTestNoParam();
+	virtual void RunBPTestNoParam_Implementation() {}
+};

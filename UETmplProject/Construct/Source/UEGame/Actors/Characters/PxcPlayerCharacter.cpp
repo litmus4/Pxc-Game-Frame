fac4a4b83fb5d560c2e088172413db2b16f73b71
litmus4 <scrudlds@163.com>
2022-08-17 23:07:26 +0800

    继续PxcPlayerCharacter的Move部分

--- UETmplProject/Construct/Source/UEGame/Actors/Characters/PxcPlayerCharacter.cpp
+++ UETmplProject/Construct/Source/UEGame/Actors/Characters/PxcPlayerCharacter.cpp
@@ -4,6 +4,7 @@
 #include "Actors/Characters/PxcPlayerCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 APxcPlayerCharacter::APxcPlayerCharacter()
 {
@@ -13,6 +14,13 @@ APxcPlayerCharacter::APxcPlayerCharacter()
 
 	m_pCameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
 	m_pCameraComp->SetupAttachment(m_pSpringArmComp);
+
+	m_bRootMotion = false;
+	m_eAnimBPType = EAnimBPType::DirectionTurned;
+	m_vTurn.Set(0.0, 0.0, 0.0);
+	m_eMotionState = EMotionState::Idle;
+	m_v2Axis.Set(0.0, 0.0);
+	m_v2LastAxis.Set(0.0, 0.0);
 }
 
 void APxcPlayerCharacter::BeginPlay()
@@ -40,10 +48,76 @@ void APxcPlayerCharacter::SetupPlayerInputComponent(UInputComponent* pPlayerInpu
 
 void APxcPlayerCharacter::OnMoveForward(float fValue)
 {
-	//
+	FVector2D v2LastAxis = m_v2Axis;
+	m_v2Axis.Y = fValue;
+	if (!m_v2Axis.IsNearlyZero() && (m_eMotionState == EMotionState::Idle ||
+		m_eMotionState == EMotionState::EndMove))
+	{
+		m_eMotionState = EMotionState::StartMove;
+		if (!m_bRootMotion)
+			m_v2LastAxis.Set(0.0, 0.0);
+	}
+	else if (m_v2Axis.IsNearlyZero() && (m_eMotionState == EMotionState::Move ||
+		m_eMotionState == EMotionState::StartMove))
+	{
+		m_eMotionState = EMotionState::EndMove;
+		if (!m_bRootMotion)
+			m_v2LastAxis = v2LastAxis;
+	}
+
+	if (m_eAnimBPType == EAnimBPType::DirectionTurned)
+	{
+		FRotator&& rCtrlRot = GetControlRotation();
+		FVector&& vForward = UKismetMathLibrary::GetForwardVector(FRotator(0.0, rCtrlRot.Yaw, 0.0));
+		FVector&& vRight = UKismetMathLibrary::GetRightVector(FRotator(0.0, rCtrlRot.Yaw, 0.0));
+		if (m_eMotionState == EMotionState::EndMove && !m_bRootMotion)
+			m_vTurn = vForward * m_v2LastAxis.Y + vRight * m_v2LastAxis.X;
+		else
+			m_vTurn =  vForward * m_v2Axis.Y + vRight * m_v2Axis.X;
+	}
+
+	if (!m_bRootMotion)
+	{
+		AddMovementInput(GetActorForwardVector() *
+			(m_eAnimBPType == EAnimBPType::DirectionTurned ? 1.0f : fValue));
+	}
 }
 
 void APxcPlayerCharacter::OnMoveRight(float fValue)
 {
-	//
+	FVector2D v2LastAxis = m_v2Axis;
+	m_v2Axis.Y = fValue;
+	if (!m_v2Axis.IsNearlyZero() && (m_eMotionState == EMotionState::Idle ||
+		m_eMotionState == EMotionState::EndMove))
+	{
+		m_eMotionState = EMotionState::StartMove;
+		if (!m_bRootMotion)
+			m_v2LastAxis.Set(0.0, 0.0);
+	}
+	else if (m_v2Axis.IsNearlyZero() && (m_eMotionState == EMotionState::Move ||
+		m_eMotionState == EMotionState::StartMove))
+	{
+		m_eMotionState = EMotionState::EndMove;
+		if (!m_bRootMotion)
+			m_v2LastAxis = v2LastAxis;
+	}
+
+	if (m_eAnimBPType == EAnimBPType::DirectionTurned)
+	{
+		FRotator&& rCtrlRot = GetControlRotation();
+		FVector&& vForward = UKismetMathLibrary::GetForwardVector(FRotator(0.0, rCtrlRot.Yaw, 0.0));
+		FVector&& vRight = UKismetMathLibrary::GetRightVector(FRotator(0.0, rCtrlRot.Yaw, 0.0));
+		if (m_eMotionState == EMotionState::EndMove && !m_bRootMotion)
+			m_vTurn = vForward * m_v2LastAxis.Y + vRight * m_v2LastAxis.X;
+		else
+			m_vTurn = vForward * m_v2Axis.Y + vRight * m_v2Axis.X;
+	}
+	
+	if (!m_bRootMotion)
+	{
+		if (m_eAnimBPType == EAnimBPType::DirectionTurned)
+			AddMovementInput(GetActorForwardVector() * 1.0f);
+		else
+			AddMovementInput(GetActorRightVector() * fValue);
+	}
 }
--- UETmplProject/Construct/Source/UEGame/Actors/Characters/PxcPlayerCharacter.h
+++ UETmplProject/Construct/Source/UEGame/Actors/Characters/PxcPlayerCharacter.h
@@ -6,6 +6,22 @@
 #include "Actors/Characters/PxcGraphCharacter.h"
 #include "PxcPlayerCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class EAnimBPType : uint8
+{
+	DirectionTurned,
+	ControllerTurned
+};
+
+UENUM(BlueprintType)
+enum class EMotionState : uint8
+{
+	Idle = 0,
+	StartMove,
+	Move,
+	EndMove
+};
+
 class UCameraComponent;
 class USpringArmComponent;
 
@@ -29,6 +45,18 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components)
 	USpringArmComponent* m_pSpringArmComp;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Animation)
+	bool m_bRootMotion;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Animation)
+	EAnimBPType m_eAnimBPType;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector m_vTurn;
+
+	UPROPERTY(BlueprintReadWrite)
+	EMotionState m_eMotionState;
+
 public:
 	virtual void Tick(float fDeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* pPlayerInputComponent) override;
@@ -36,4 +64,7 @@ public:
 protected:
 	void OnMoveForward(float fValue);
 	void OnMoveRight(float fValue);
+
+	FVector2D m_v2Axis;
+	FVector2D m_v2LastAxis;
 };
